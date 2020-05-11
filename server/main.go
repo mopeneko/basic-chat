d2b079c306faea6d3c2ac2170abf3851755e8f67
mopeneko <26901342+mopeneko@users.noreply.github.com>
2020-05-11 18:45:20 +0900

    Implement server

--- server/main.go
+++ server/main.go
@@ -1,7 +1,86 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	"unicode/utf8"
+)
+
+type Request struct {
+	Message string `json:"message"`
+}
+
+const MaxMessageLength = 140
+
+var wsUpgrader = websocket.Upgrader{}
+
+var clients = make(map[*websocket.Conn]bool)
+
+var broadcaster = make(chan Request)
 
 func main() {
-	fmt.Println("Hello, world!")
+	go startBroadcasting()
+
+	http.HandleFunc("/ws", ws)
+
+	fmt.Println("WebSocket is working on http://localhost:4000/ws")
+	log.Fatal(http.ListenAndServe(":4000", nil))
+}
+
+func ws(w http.ResponseWriter, r *http.Request) {
+	c, err := wsUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Error: %+v\n", err)
+		return
+	}
+
+	defer c.Close()
+	defer delete(clients, c)
+
+	clients[c] = true
+
+	hello := Request{
+		Message: fmt.Sprintf("[運営ちゃん] 新しい参加者が現れましたっ。これで%d人ですっ！", len(clients)),
+	}
+	broadcaster <- hello
+
+	for {
+		request := Request{}
+		err := c.ReadJSON(&request)
+
+		if err != nil {
+			log.Printf("Error: %+v\n", err)
+
+			bye := Request{
+				Message: "[運営ちゃん] 誰かが退出したようです。。。",
+			}
+			broadcaster <- bye
+
+			break
+		}
+
+		broadcaster <- request
+	}
+}
+
+func startBroadcasting() {
+	for {
+		request := <-broadcaster
+
+		if utf8.RuneCountInString(request.Message) > MaxMessageLength {
+			continue
+		}
+
+		for client := range clients {
+			err := client.WriteJSON(request)
+
+			if err != nil {
+				log.Printf("Error: %+v\n", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
 }
